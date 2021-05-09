ad16a0e9be4cc26a8ebca4832bcf348248e20636
Nikolay <53932721+vnv@users.noreply.github.com>
2021-05-09 14:39:10 +0300

    Refactoring

--- Course 1 - Basics of statistics/Step_3_8_1.py
+++ Course 1 - Basics of statistics/Step_3_8_1.py
@@ -1,21 +1,14 @@
 # https://stepik.org/lesson/8090/step/2?unit=1369
 # Интерпретация результатов регрессионного анализа с несколькими независимыми переменными
 
-import pandas as pd, numpy as np, seaborn as sns
 import matplotlib.pyplot as plt
-import scipy.stats as stats
-from mpl_toolkits.mplot3d import Axes3D
+import pandas as pd
 
 data = pd.read_csv('http://d396qusza40orc.cloudfront.net/statistics/lec_resources/states.csv')
 
-data_crop = data[['white', 'hs_grad', 'poverty']]
-data_crop.head()
-white, hs_grad, poverty = [column for column in data_crop.values.T]
-
 fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(xs=white, ys=poverty, zs=hs_grad)
-
+ax = fig.add_subplot(111, projection="3d")
+ax.scatter(xs=data.white, ys=data.poverty, zs=data.hs_grad)
 ax.set_xlabel('White(%)')
 ax.set_ylabel('Poverty(%)')
 ax.set_zlabel('Higher education(%)')
